7f48fae22831bff7499eba1d111a511ed5e88fe9
csincl10 <63428029+Sinclair49@users.noreply.github.com>
2021-10-03 13:18:37 -0400

    sfadfaf

--- itis3135/scripts/activity6.js
+++ itis3135/scripts/activity6.js
@@ -1,79 +1,69 @@
 var entry = parseInt(prompt("The Charming Sloth would like your number (from 1 to 10)"));
 
- while(!validateEntry(entry)){
-   
- entry = parseInt(prompt("Invalid entry: please enter a number from 1 to 10"));
- 
- }
- var absval = Math.abs(entry);
-var temp=Math.round(absval);
- getShape(temp);
+while (!validateEntry(entry)) {
 
-function validateEntry(entry){
-	if (isNaN(entry)){
+    entry = parseInt(prompt("Invalid entry: please enter a number from 1 to 10"));
 
-return false;
 }
 var absval = Math.abs(entry);
-var temp=Math.round(absval);
+var temp = Math.round(absval);
+getShape(temp);
 
-if (temp<0 || temp>10){
-	return false;
-	}
-	return true;
+function validateEntry(entry) {
+    if (isNaN(entry)) {
+
+        return false;
+    }
+    var absval = Math.abs(entry);
+    var temp = Math.round(absval);
+
+    if (temp < 0 || temp > 10) {
+        return false;
+    }
+    return true;
 }
-function getShape(side){
-	if(side==0)
-	{
-		alert("No sides, not a polygon!");
-	}
-	if(side==1)
-	{
-		alert("henagon");
-	}
-	
-	if(side==2)
-	{
-		alert("digon");
-	}
-	
-	if(side==3)
-	{
-		alert("trigon");
-	}
 
-		if(side==4)
-	{
-		alert("tetragon");
-	}
-	
-	if(side==5)
-	{
-		alert("pentagon");
-	}
-	
-	if(side==6)
-	{
-		alert("hexagon");
-	}
+function getShape(side) {
+    if (side == 0) {
+        alert("No sides, not a polygon!");
+    }
+    if (side == 1) {
+        alert("henagon");
+    }
 
-		if(side==7)
-	{
-		alert("heptagon");
-	}
-	
-	if(side==8)
-	{
-		alert("octagon");
-	}
-	
-	if(side==9)
-	{
-		alert("enneagon");
-	}
-	
-	if(side==10)
-	{
-		alert("decagon");
-	}
+    if (side == 2) {
+        alert("digon");
+    }
+
+    if (side == 3) {
+        alert("trigon");
+    }
+
+    if (side == 4) {
+        alert("tetragon");
+    }
+
+    if (side == 5) {
+        alert("pentagon");
+    }
+
+    if (side == 6) {
+        alert("hexagon");
+    }
+
+    if (side == 7) {
+        alert("heptagon");
+    }
+
+    if (side == 8) {
+        alert("octagon");
+    }
+
+    if (side == 9) {
+        alert("enneagon");
+    }
+
+    if (side == 10) {
+        alert("decagon");
+    }
 }
