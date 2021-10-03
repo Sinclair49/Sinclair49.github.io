2dba0296e08557512bf891a37f0a186a473383cf
csincl10 <63428029+Sinclair49@users.noreply.github.com>
2021-10-03 14:08:23 -0400

    asfd

--- itis3135/scripts/activity7.js
+++ itis3135/scripts/activity7.js
@@ -2,157 +2,207 @@ var prevCalc = 0;
 var calc = "";
 
 window.onload = function() {
-    document.getElementById('btn0').onclick = showNum;
-    document.getElementById('btn1').onclick = showNum;
-    document.getElementById('btn2').onclick = showNum;
-    document.getElementById('btn3').onclick = showNum;
-    document.getElementById('btn4').onclick = showNum;
-    document.getElementById('btn5').onclick = showNum;
-    document.getElementById('btn6').onclick = showNum;
-    document.getElementById('btn7').onclick = showNum;
-    document.getElementById('btn8').onclick = showNum;
-    document.getElementById('btn9').onclick = showNum;
-    document.getElementById('btnDecimal').onclick = showNum;
-    document.getElementById('btnPlus').onclick = add;
-    document.getElementById('btnMinus').onclick = subtract;
-    document.getElementById('btnTimes').onclick = multiply;
-    document.getElementById('btnDivide').onclick = divide;
-    document.getElementById('btnReset').onclick = clear;
-    document.getElementById('btnCalc').onclick = calculate;
+    document.getElementById('0').onclick = showNumber;
+    document.getElementById('1').onclick = showNumber;
+    document.getElementById('2').onclick = showNumber;
+    document.getElementById('3').onclick = showNumber;
+    document.getElementById('4').onclick = showNumber;
+    document.getElementById('5').onclick = showNumber;
+    document.getElementById('6').onclick = showNumber;
+    document.getElementById('7').onclick = showNumber;
+    document.getElementById('8').onclick = showNumber;
+    document.getElementById('9').onclick = showNumber;
+    document.getElementById('Decimal').onclick = showNumber;
+    document.getElementById('Plus').onclick = add;
+    document.getElementById('Minus').onclick = subtract;
+    document.getElementById('Times').onclick = multiply;
+    document.getElementById('Divide').onclick = divide;
+    document.getElementById('Clear').onclick = clear;
+    document.getElementById('Calc').onclick = calculate;
 }
 
 
-
-function showNum() {
+function showNumber() {
     document.frmCalc.txtNumber.value += this.value;
 }
 
- 
-function decrement() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            num--;
-            document.frmCalc.txtNumber.value = num;
-        }
-}
-
-function increment() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            num++;
-            document.frmCalc.txtNumber.value = num;
-        }
-}
-
-function sqrt() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            num = Math.sqrt(num);
-            document.frmCalc.txtNumber.value = num;
-        }
-}
-
-function square() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            num = Math.pow(num, 2);
-            document.frmCalc.txtNumber.value = num;
-        }
-}
-
-function floor() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            num = Math.floor(num);
-            document.frmCalc.txtNumber.value = num;
-        }
-}
 
 
 function round() {
     var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            num = Math.round(num);
-            document.frmCalc.txtNumber.value = num;
-        }
+    if (!(isNaN(num))) {
+        num = Math.round(num);
+        document.frmCalc.txtNumber.value = num;
+    }
 }
- 
+
 function add() {
     var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            prevCalc = num;
-            document.frmCalc.txtNumber.value = "";
-            calc = "Add";
-        }
+    if (!(isNaN(num))) {
+        prevCalc = num;
+        document.frmCalc.txtNumber.value = "";
+        calc = "Add";
+    }
 }
 
 function subtract() {
     var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            prevCalc = num;
-            document.frmCalc.txtNumber.value = "";
-            calc = "Subtract";
-        }
+    if (!(isNaN(num))) {
+        prevCalc = num;
+        document.frmCalc.txtNumber.value = "";
+        calc = "Subtract";
+    }
 }
 
 
 function multiply() {
     var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            prevCalc = num;
-            document.frmCalc.txtNumber.value = "";
-            calc = "Multiply";
-        }
+    if (!(isNaN(num))) {
+        prevCalc = num;
+        document.frmCalc.txtNumber.value = "";
+        calc = "Multiply";
+    }
 }
 
 function divide() {
     var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            prevCalc = num;
-            document.frmCalc.txtNumber.value = "";
-            calc = "Divide";
-        }
+    if (!(isNaN(num))) {
+        prevCalc = num;
+        document.frmCalc.txtNumber.value = "";
+        calc = "Divide";
+    }
 }
 
-function power() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            prevCalc = num;
-            document.frmCalc.txtNumber.value = "";
-            calc = "Power";
-        }
-}
 
-     
 function calculate() {
     var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            if (calc == "Add"){
-                var total = prevCalc + num;
-                document.frmCalc.txtNumber.value = total;
-            }
-            if (calc == "Subtract"){
-                var total = prevCalc - num;
-                document.frmCalc.txtNumber.value = total;
-            }
-            if (calc == "Multiply"){
-                var total = prevCalc * num;
-                document.frmCalc.txtNumber.value = total;
-            }
-            if (calc == "Divide"){
-                var total = prevCalc / num;
-                document.frmCalc.txtNumber.value = total;
-            }
-            if (calc == "Power"){
-                var total = Math.pow(prevCalc, num);
-                document.frmCalc.txtNumber.value = total;
-            }
-            
-        
-}}
+    if (!(isNaN(num))) {
+        if (calc == "Add") {
+            var total = prevCalc + num;
+            document.frmCalc.txtNumber.value = total;
+        }
+        if (calc == "Subtract") {
+            var total = prevCalc - num;
+            document.frmCalc.txtNumber.value = total;
+        }
+        if (calc == "Multiply") {
+            var total = prevCalc * num;
+            document.frmCalc.txtNumber.value = total;
+        }
+        if (calc == "Divide") {
+            var total = prevCalc / num;
+            document.frmCalc.txtNumber.value = total;
+        }
+
+    }
+}
 
 function clear() {
-	document.frmCalc.txtNumber.value = "";
-	prevCalc = 0;
-	calc = "";
+    document.frmCalc.txtNumber.value = "";
+    prevCalc = 0;
+    calc = "";
+}
+var prevCalc = 0;
+var calc = "";
+
+window.onload = function() {
+    document.getElementById('0').onclick = showNumber;
+    document.getElementById('1').onclick = showNumber;
+    document.getElementById('2').onclick = showNumber;
+    document.getElementById('3').onclick = showNumber;
+    document.getElementById('4').onclick = showNumber;
+    document.getElementById('5').onclick = showNumber;
+    document.getElementById('6').onclick = showNumber;
+    document.getElementById('7').onclick = showNumber;
+    document.getElementById('8').onclick = showNumber;
+    document.getElementById('9').onclick = showNumber;
+    document.getElementById('Decimal').onclick = showNumber;
+    document.getElementById('Plus').onclick = add;
+    document.getElementById('Minus').onclick = subtract;
+    document.getElementById('Times').onclick = multiply;
+    document.getElementById('Divide').onclick = divide;
+    document.getElementById('Clear').onclick = clear;
+    document.getElementById('Calc').onclick = calculate;
+}
+
+
+function showNumber() {
+    document.frmCalc.txtNumber.value += this.value;
+}
+
+
+
+function round() {
+    var num = parseFloat(document.frmCalc.txtNumber.value);
+    if (!(isNaN(num))) {
+        num = Math.round(num);
+        document.frmCalc.txtNumber.value = num;
+    }
+}
+
+function add() {
+    var num = parseFloat(document.frmCalc.txtNumber.value);
+    if (!(isNaN(num))) {
+        prevCalc = num;
+        document.frmCalc.txtNumber.value = "";
+        calc = "Add";
+    }
+}
+
+function subtract() {
+    var num = parseFloat(document.frmCalc.txtNumber.value);
+    if (!(isNaN(num))) {
+        prevCalc = num;
+        document.frmCalc.txtNumber.value = "";
+        calc = "Subtract";
+    }
+}
+
+
+function multiply() {
+    var num = parseFloat(document.frmCalc.txtNumber.value);
+    if (!(isNaN(num))) {
+        prevCalc = num;
+        document.frmCalc.txtNumber.value = "";
+        calc = "Multiply";
+    }
+}
+
+function divide() {
+    var num = parseFloat(document.frmCalc.txtNumber.value);
+    if (!(isNaN(num))) {
+        prevCalc = num;
+        document.frmCalc.txtNumber.value = "";
+        calc = "Divide";
+    }
+}
+
+
+function calculate() {
+    var num = parseFloat(document.frmCalc.txtNumber.value);
+    if (!(isNaN(num))) {
+        if (calc == "Add") {
+            var total = prevCalc + num;
+            document.frmCalc.txtNumber.value = total;
+        }
+        if (calc == "Subtract") {
+            var total = prevCalc - num;
+            document.frmCalc.txtNumber.value = total;
+        }
+        if (calc == "Multiply") {
+            var total = prevCalc * num;
+            document.frmCalc.txtNumber.value = total;
+        }
+        if (calc == "Divide") {
+            var total = prevCalc / num;
+            document.frmCalc.txtNumber.value = total;
+        }
+
+    }
+}
+
+function clear() {
+    document.frmCalc.txtNumber.value = "";
+    prevCalc = 0;
+    calc = "";
 }
